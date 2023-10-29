65c0248a3af889306d512d17868f55f705873b8a
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-10-29 13:48:03 +0700

    notification animation text

--- styles/js/index.js
+++ styles/js/index.js
@@ -1,32 +1,54 @@
 const NavDOM = document.querySelector("#nav");
 const HeaderDOM = document.querySelector("#header");
+const NotificationDOM = document.querySelector("#notification");
+
+let NotificationDOMPosition = NotificationDOM.getBoundingClientRect().top;
 
 function onLoadPage() {
-  onLoadHeader()
+  onLoadHeader();
 }
 
-// FUNCTION FOR HANDLE HEADER ANIMATION
+// FUNCTION FOR HANDLE SECTION HEADER ANIMATION
 function onLoadHeader() {
   // WIDTH HEADER LEFT - HEADER RIGHT
-  HeaderDOM.querySelector(".header-left").style.width = "8%"
-  HeaderDOM.querySelector(".header-right").style.width = "92%"
-  HeaderDOM.querySelector(".header__main-text").style.transform = "translateY(0px)"
-  HeaderDOM.querySelector(".header__secondary-text").style.transform = "translateY(0px)"
+  HeaderDOM.querySelector(".header-left").style.width = "8%";
+  HeaderDOM.querySelector(".header-right").style.width = "92%";
+  HeaderDOM.querySelector(".header__main-text").style.transform =
+    "translateY(0px)";
+  HeaderDOM.querySelector(".header__secondary-text").style.transform =
+    "translateY(0px)";
+}
+
+// FUNCTION FOR HANDLE SECTION NOTIFICATION
+function onLoadNotification() {
+  NotificationDOM.querySelector(".notification__main-text").style.transform =
+    "translateY(0px)";
+  NotificationDOM.querySelector(
+    ".notification__secondary-text"
+  ).style.transform = "translateY(0px)";
 }
 
 // NAV: WHEN SCROLL CHANGE BACKGROUND COLOR AND TEXT COLOR
-window.addEventListener("scroll", function () {
+document.addEventListener("scroll", function () {
+  // HANDLE NAV BACKGROUND
   if (window.scrollY > 100) {
-    NavDOM.querySelector(".-translate-y-full").style.transform = "translateY(0px)"
+    NavDOM.querySelector(".-translate-y-full").style.transform =
+      "translateY(0px)";
     const navAnchorDOM = NavDOM.querySelectorAll(".nav-anchor");
     navAnchorDOM.forEach((anchor) => {
-      anchor.style.color = "#1E1E1E"
+      anchor.style.color = "#1E1E1E";
     });
-  } else {
-    NavDOM.querySelector(".-translate-y-full").style.transform = "translateY(-100%)"
+  }
+  if (window.scrollY < 100) {
+    NavDOM.querySelector(".-translate-y-full").style.transform =
+      "translateY(-100%)";
     const navAnchorDOM = NavDOM.querySelectorAll(".nav-anchor");
     navAnchorDOM.forEach((anchor) => {
-      anchor.style.color = "#EDEDED"
+      anchor.style.color = "#EDEDED";
     });
+  }
+
+  if (window.scrollY > NotificationDOMPosition - 450) {
+    onLoadNotification();
   }
 });
